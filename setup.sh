a97c1682a0b4d68f9216136d28a239b33eaab8f6
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-11-15 16:35:17 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,4 +13,11 @@ python3 -m pip install --upgrade pip
 python3 --version
 
 python -m pip install spacy
+
+# Create the models directory if it doesn't exist
+mkdir -p /home/user/dir/pages/models
+
+# Set the custom download location
+export SPACY_DOWNLOAD_LOCATION=/home/user/dir/pages/models
+
 python -m spacy download en_core_web_md
