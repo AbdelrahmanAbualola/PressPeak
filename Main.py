eb9e87471190919676790e27de1b049769af8a65
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-11-15 18:20:40 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -141,7 +141,7 @@ with st.sidebar:
         st.session_state['websites'] = []
         
     with st.popover('Websites',use_container_width=True):
-        websites= st_tags(label='Websites', text='Press enter to add more', maxtags = 10, key='websites',value=st.session_state['websites'])
+        websites= st_tags(label='Websites', text='Only Search these websites', maxtags = 10, key='websites',value=st.session_state['websites'])
         
     if st.session_state['websites'] != websites:
         st.session_state['websites'] = websites
