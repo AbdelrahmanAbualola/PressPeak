c16725928da0a5498e44980f3478a681ef080fb0
AbdelrahmanAbualola <115497203+AbdelrahmanAbualola@users.noreply.github.com>
2024-11-15 16:09:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-python -m pip install --upgrade pip
+# Update package list and upgrade system packages
+sudo apt update && sudo apt upgrade -y
 
-python -m spacy download en_core_web_md
+# Install Python 3 and pip if not installed
+sudo apt install python3 python3-pip -y
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
+# Verify Python version
+python3 --version
